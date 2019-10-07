10cef426729fc2d1236cbf65b4aa365159f6a5a9
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-10-07 19:38:26 +0300

    Created initial 2 task project files

---
+++ LB-2/cmake-build-debug/CMakeFiles/LB_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LB_2.dir/main.cpp.o"
+  "LB_2.pdb"
+  "LB_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LB_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
