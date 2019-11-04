f25b4eb3e12a7c0cf9b36339eb88bb3dcce392af
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-04 23:26:16 +0200

    Progress save

--- LD-2/car.go
+++ LD-2/car.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
+
 type Car struct {
 	Brand    string
 	MakeYear int
 	Mileage  float32
+}
+
+func (c *Car) print() {
+	fmt.Printf("%s %d %f\n", c.Brand, c.MakeYear, c.Mileage)
+}
+
+func (c *Car) getNumber() int {
+	number := c.MakeYear + int(c.Mileage) + int(c.Brand[0])
+	return c.fibonacci(number % 40)
+}
+
+func (c *Car) fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return c.fibonacci(n-1) + c.fibonacci(n-2)
 }
--- LD-2/main.go
+++ LD-2/main.go
@@ -2,19 +2,67 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"sync"
 )
 
+const filePath string = "data/IFF-7_2_MilisiunasJ_L1_dat_2.json"
+const filterCondition int = 50000
+const bufferSize int = 5
+
 func main() {
-	filePath := "data/IFF-7_2_MilisiunasJ_L1_dat_1.json"
 	data := ReadData(filePath)
 	cars := ParseJsonData(data)
+	//n := len(cars)
 
-	print(cars[0].Brand)
+	group := sync.WaitGroup{}
+	group.Add(4)
 
+	// Channel creation
+	dataChan := make(chan Car, 100)
+	filtered := make(chan Car, 100)
+
+	// Routines starting
+	go WorkerRoutine(dataChan, filtered, &group)
+	go WorkerRoutine(dataChan, filtered, &group)
+	go WorkerRoutine(dataChan, filtered, &group)
+	go WorkerRoutine(dataChan, filtered, &group)
+	for _, car := range cars {
+		dataChan <- car // Sends car to data channel
+	}
+
+	close(dataChan)
+
+	// TODO: delete after creating results routine work
+	go func() {
+		counter := 0
+		for car := range filtered {
+			car.print()
+			counter++
+			fmt.Println(counter)
+		}
+	} ()
+
+	group.Wait()
+	close(filtered)
+
+	fmt.Println("Work finished!")
 }
 
+// Receives cars from data channel, filters it, sends to filtered channel
+func WorkerRoutine(data <-chan Car, filtered chan<- Car, group *sync.WaitGroup) {
+	defer group.Done()
+
+	for car := range data {
+		if car.getNumber() <= filterCondition {
+			filtered <- car
+		}
+	}
+}
+
+// Parse json data to Car array
 func ParseJsonData(jsonData []byte) []Car {
 	var cars []Car
 	err := json.Unmarshal([]byte(jsonData), &cars)
@@ -25,6 +73,7 @@ func ParseJsonData(jsonData []byte) []Car {
 	return cars
 }
 
+// Reads data in bytes from given file
 func ReadData(filePath string) []byte {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
